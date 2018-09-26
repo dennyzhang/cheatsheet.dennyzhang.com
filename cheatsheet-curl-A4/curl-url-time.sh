e7dc4a26268fb2c264b6efbc994cc78dac7b4d04
Denny Zhang <denny.zhang001@gmail.com>
2018-09-25 17:45:00 -0700

    add curl pdf

--- cheatsheet-curl-A4/curl-url-time.sh
+++ cheatsheet-curl-A4/curl-url-time.sh
@@ -7,6 +7,8 @@
 ## Description :
 ## --
 ## Created : <2018-09-25>
-## Updated: Time-stamp: <2018-09-25 17:39:40>
+## Updated: Time-stamp: <2018-09-25 17:43:35>
 ##-------------------------------------------------------------------
-curl -s -w '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nAppCon time:\t%{time_appconnect}\nRedirect time:\t%{time_redirect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' -o /dev/null https://www.google.com
+curl -s -w \
+     '\nLookup time:\t%{time_namelookup}\nConnect time:\t%{time_connect}\nAppCon time:\t%{time_appconnect}\nRedirect time:\t%{time_redirect}\nPreXfer time:\t%{time_pretransfer}\nStartXfer time:\t%{time_starttransfer}\n\nTotal time:\t%{time_total}\n' \
+     -o /dev/null https://www.google.com
