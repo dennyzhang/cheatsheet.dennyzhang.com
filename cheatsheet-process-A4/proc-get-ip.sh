a43dca229f519ca2aed6d63fccd2a202152af8d6
Denny Zhang <denny.zhang001@gmail.com>
2018-11-04 23:04:32 -0800

    add more cheatsheet

--- cheatsheet-process-A4/proc-get-ip.sh
+++ cheatsheet-process-A4/proc-get-ip.sh
@@ -7,9 +7,9 @@
 ## Author : Denny <https://www.dennyzhang.com/contact>
 ## Description :
 ## https://unix.stackexchange.com/questions/365225/how-to-get-the-ipv4-address-for-an-interface-from-proc
-## https://cheatsheet.dennyzhang.com/cheatsheet-procfs-A4
+## https://cheatsheet.dennyzhang.com/cheatsheet-process-A4
 ## --
 ## Created : <2018-10-06>
-## Updated: Time-stamp: <2018-10-06 19:07:35>
+## Updated: Time-stamp: <2018-11-04 22:53:26>
 ##-------------------------------------------------------------------
 cat /proc/net/fib_trie | grep "|--"   | egrep -v "0.0.0.0| 127."
