5980ec8d3c3795e7532529e0b624041fa54098cb
Denny Zhang <denny.zhang001@gmail.com>
2018-11-22 18:30:10 +0800

    listen port

---
+++ cheatsheet-process-A4/proc-listen-ports.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+##  @copyright 2018 DennyZhang.com
+## Licensed under MIT 
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: hello
+## Author : Denny <https://www.dennyzhang.com/contact>
+## Description :
+## --
+## Created : <2018-10-06>
+## Updated: Time-stamp: <2018-11-22 18:29:08>
+##-------------------------------------------------------------------
+sudo lsof -p $pid | grep -i listen
